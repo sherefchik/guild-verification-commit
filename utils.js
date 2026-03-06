dbb82b84a04706d819f0ef20a33518493cfd5c84
sherefchik <35901015+sherefchik@users.noreply.github.com>
2026-03-06 21:16:05 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T18:16:02.989Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T18:16:04.696Z
+Commit: Add export functionality for reports
