79922620b81fc9e0a70ef853f4b224da3613d0e4
sherefchik <35901015+sherefchik@users.noreply.github.com>
2026-03-06 21:16:49 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T18:16:38.540Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T18:16:49.470Z
+Commit: Add export functionality for reports
