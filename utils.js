f80051f65f090eb6cb5ab2a52ef425abf77b2891
sherefchik <35901015+sherefchik@users.noreply.github.com>
2026-03-06 21:16:55 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T18:16:53.183Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T18:16:55.346Z
+Commit: Fix memory leak in event handlers
