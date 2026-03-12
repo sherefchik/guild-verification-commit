495c73dcb585c01c739c8a8d5830585fe858d4ad
sherefchik <35901015+sherefchik@users.noreply.github.com>
2026-03-12 20:55:12 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T17:55:00.792Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-12T17:55:12.433Z
+Commit: Resolve null pointer exception in user service
