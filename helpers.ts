acc967cfa630694c4d884ec125743b9f21d5f84c
sherefchik <35901015+sherefchik@users.noreply.github.com>
2026-03-12 20:55:00 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T17:54:43.282Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-12T17:55:00.792Z
+Commit: Add export functionality for reports
